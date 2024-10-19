1d08507d094bfbff018b2e12904f4dbe27ad5378
Arthur Vieira <arthur.devbr@gmail.com>
2024-10-19 17:22:52 +0100

    primitve types

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //Variables
+    int age = 28;
+    String name = "Arthur";
+    var role = "Software Developer";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+    //Primitive types
+
+    //whole number
+    byte
+    short
+    int
+    long
+    //decimal number
+    float
+    double
+
+    //Words and phrases
+    //-String (Phrases and words) - "Jorgin me esmpresta a 12"
+    //-char (Single letter) - 'a'
+
+    //Boolean
+    //true or false
+
 }
+
