d86c63d05771b30b63ae71f31482401ef3c78363
Arthur Vieira <arthur.devbr@gmail.com>
2024-11-13 14:51:56 +0000

    ex3 codicional sao multiplos

--- src/java_curso/Main.java
+++ src/java_curso/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int numero;
 
         System.out.println("Digite um número:");
-        numero = sc.nextInt();
+        int a = sc.nextInt();
+        System.out.println("Digite outro número:");
+        int b = sc.nextInt();
 
-        if (numero % 2 == 0) {
-            System.out.println("PAR");
+
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("São múltiplos");
         } else {
-            System.out.println("IMPAR");
+            System.out.println("Não são múltiplos.");
         }
 
 
