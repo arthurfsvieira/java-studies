a329948e4fabe8b65f5b4653f2d1c08ddb8bc967
Arthur Vieira <arthur.devbr@gmail.com>
2024-11-19 12:39:03 +0000

    Update Main.java

--- src/java_curso/Main.java
+++ src/java_curso/Main.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int senha = sc.nextInt();
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
 
-        while (senha != 2002) {
-            System.out.println("Senha inválida");
-            senha = sc.nextInt();
+        int tipo = sc.nextInt();
+
+        while (tipo != 4){
+            if (tipo == 1){
+                alcool = alcool + 1;
+            } else if (tipo == 2) {
+                gasolina = gasolina + 1;
+            } else if (tipo == 3) {
+                diesel = diesel + 1;
+            }
+            tipo = sc.nextInt();
         }
-        System.out.println("Acesso liberado.");
+        System.out.println("Muito obrigado");
+        System.out.println("Álcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
 
         sc.close();
 
