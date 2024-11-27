83afab28765c56ac5b3d540b570ec7fad4c4c163
Arthur Vieira <arthur.devbr@gmail.com>
2024-11-27 23:07:16 +0000

    for ex1

--- src/java_curso/Main.java
+++ src/java_curso/Main.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        int x = 3;
-        int y = 0;
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
 
-        for (int i=0; i<x; i++){
-            System.out.print(i + ",");
-            y = y + 5;
-            System.out.println(y);
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
         }
+        
+        sc.close();
 
     }
 }
-// lembrete: realizar exercícios para fixação 
