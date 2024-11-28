4633614c5b6bd5729380a53048c359ed919035a0
Arthur Vieira <arthur.devbr@gmail.com>
2024-11-28 11:26:01 +0000

    for ex2

--- src/java_curso/Main.java
+++ src/java_curso/Main.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+        int n = sc.nextInt();
 
-        for (int i = 1; i <= x; i++) {
-            if (i % 2 != 0) {
-                System.out.println(i);
+        int in = 0;
+        int out = 0;
+
+        for (int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in++;
+            } else {
+                out++;
             }
         }
-        
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
         sc.close();
 
     }
 }
+// aula 47 - Estruturas de repetição;
