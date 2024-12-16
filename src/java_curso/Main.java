63af2bf136c9b99beb8c7478425362c8a8269afc
Arthur Vieira <arthur.devbr@gmail.com>
2024-12-16 13:44:53 +0000

    Update Main.java

--- src/java_curso/Main.java
+++ src/java_curso/Main.java
@@ -6,23 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int in = 0;
-        int out = 0;
 
-        for (int i=0; i<n; i++){
-            int x = sc.nextInt();
-            if (x >= 10 && x <= 20) {
-                in++;
-            } else {
-                out++;
-            }
+        int n = sc.nextInt();
+
+        int fat = 1;
+
+        for (int i=1; i<=n; i++) {
+            fat = fat * i;
+
         }
-        System.out.println(in + " in");
-        System.out.println(out + " out");
+        System.out.println(fat);
 
         sc.close();
 
     }
 }
-//aula 53
